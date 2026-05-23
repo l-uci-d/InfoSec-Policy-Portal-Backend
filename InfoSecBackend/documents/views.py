0e5b20b16649c8972df1c36f3163ebd404175c96
gekiiMei <gekimeshi@gmail.com>
2026-05-23 20:49:56 +0800

    fixed missing misc_title param

--- InfoSecBackend/documents/views.py
+++ InfoSecBackend/documents/views.py
@@ -85,9 +85,9 @@ def create_update_document(request):
             new_subsect.save()
     
     if updating:
-        create_notif(actor=User.objects.get(id=int(curr_user_id)), action="updated", document=new_doc)
+        create_notif(actor=User.objects.get(id=int(curr_user_id)), action="updated", document=new_doc, misc_title=None)
     else:
-        create_notif(actor=User.objects.get(id=int(curr_user_id)), action="created", document=new_doc)
+        create_notif(actor=User.objects.get(id=int(curr_user_id)), action="created", document=new_doc, misc_title=None)
 
     return Response(status=status.HTTP_200_OK)
 
